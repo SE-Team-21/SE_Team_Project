0d9e37b250940d4f6807137f2e80b80f33bf3337
Han Jeong Hyeon <113430214+p1432pop@users.noreply.github.com>
2023-03-15 01:13:44 +0900

    Update UNO.py

--- UNO.py
+++ UNO.py
@@ -1,6 +1,8 @@
 import pygame as pg
 import Draw_Start_Display
 import Draw_Setting_Display
+import Draw_Playing_Display
+import Draw_Stop_Display
 
 def pygame_mainloop():
     pg.init()
@@ -10,12 +12,16 @@ def pygame_mainloop():
     screen = pg.display.set_mode((screen_width, screen_height))
     
     running = [True]
-    mode = [1]
+    mode = [1, 1] # mode[0] = 다음 화면, mode[1] = 이전 화면
     while running[0]:
         if mode[0]==1:
             Draw_Start_Display.draw(screen, mode, running)
         elif mode[0]==2:
             Draw_Setting_Display.draw(screen, mode, running)
+        elif mode[0]==3:
+            Draw_Playing_Display.draw(screen, mode, running)
+        elif mode[0]==4:
+            Draw_Stop_Display.draw(screen, mode, running)
         else:
             pass
         pg.display.update()
