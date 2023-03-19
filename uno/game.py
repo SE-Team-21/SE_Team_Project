58606740c1c90546426fb214e767e45146236e23
yhcho0405 <yhcho0405@kakao.com>
2023-03-19 21:01:57 +0900

    def global constant

---
+++ uno/game.py
@@ -0,0 +1,12 @@
+from random import shuffle, choice
+from itertools import product, repeat, chain
+
+
+COLORS = ['red', 'yellow', 'green', 'blue']
+ALL_COLORS = COLORS + ['black']
+NUMBERS = list(range(10)) + list(range(1, 10))
+SPECIAL_CARD_TYPES = ['skip', 'reverse', '+2']
+COLOR_CARD_TYPES = NUMBERS + SPECIAL_CARD_TYPES * 2
+BLACK_CARD_TYPES = ['wildcard', '+4']
+CARD_TYPES = NUMBERS + SPECIAL_CARD_TYPES + BLACK_CARD_TYPES
+
