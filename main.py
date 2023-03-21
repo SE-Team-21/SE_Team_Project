c2e7734afc3f5a6cec2485b8a1d949f4f24d8e4c
Han Jeong Hyeon <113430214+p1432pop@users.noreply.github.com>
2023-03-21 20:23:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import uno.Draw_Start_Display as Draw_Start_Display
 import uno.Draw_Setting_Display as Draw_Setting_Display
 import uno.Draw_Playing_Display as Draw_Playing_Display
 import uno.Draw_Stop_Display as Draw_Stop_Display
-import uno.Constants as C
-
+import uno.constants as C
+import uno.KeySettings as K
 
 def uno_mainloop():
     pg.init()
-    pg.display.set_caption("UNO Game")
+    pg.display.set_caption("UNO Game") # 실행 창 제목
     screen = pg.display.set_mode((800,600))
+    K.load_settings()
+    for idx in range(5): # load 해온 값으로 설정 창에 있는 버튼 다시 그려주기
+        Draw_Setting_Display.Button_list[idx+4].change_text(pg.key.name(K.KEY_Settings[idx])) #Draw_Setting에 있는 Button의 index가 4~8이라서 idx+4해줌
     running = [True]
     mode = [C.START, C.START] # mode[0] = 다음 화면, mode[1] = 이전 화면
-
-    
     while running[0]:
         if mode[C.NEXT_SCREEN] == C.START:
             Draw_Start_Display.draw(screen, mode, running)
@@ -26,11 +27,7 @@ def uno_mainloop():
         else:
             pass
         pg.display.update()
-        pg.time.wait(100)
     pg.quit()
 
 if __name__ == "__main__":
     uno_mainloop()
-
-
-
