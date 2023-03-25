e2ec64d33c67b69599e5dad622d456608eb9aa56
Han Jeong Hyeon <113430214+p1432pop@users.noreply.github.com>
2023-03-26 04:51:06 +0900

    Feat: ESC key add

--- uno/KeySettings.py
+++ uno/KeySettings.py
@@ -17,12 +17,12 @@ def load_settings():
         with open("data.pickle","rb") as fr:
             KEY_Settings = pickle.load(fr)
         if(len(KEY_Settings) == 0):
-            KEY_Settings = [pg.K_UP, pg.K_LEFT, pg.K_DOWN, pg.K_RIGHT, pg.K_RETURN, pg.K_KP_ENTER]
+            KEY_Settings = [pg.K_UP, pg.K_LEFT, pg.K_DOWN, pg.K_RIGHT, pg.K_RETURN, pg.K_ESCAPE]
             with open('data.pickle', 'wb') as f:
                 pickle.dump(KEY_Settings, f)
         
             
     except: # data 파일이 없다면 기본값을 초기화 하고 만든다 (wb)
-        KEY_Settings = [pg.K_UP, pg.K_LEFT, pg.K_DOWN, pg.K_RIGHT, pg.K_RETURN, pg.K_KP_ENTER]
+        KEY_Settings = [pg.K_UP, pg.K_LEFT, pg.K_DOWN, pg.K_RIGHT, pg.K_RETURN, pg.K_ESCAPE]
         with open("data.pickle", "wb") as fw:
             pickle.dump(KEY_Settings, fw)
