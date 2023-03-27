6357a47e0530b007f33081f7804a8d325a800b7a
Han Jeong Hyeon <113430214+p1432pop@users.noreply.github.com>
2023-03-28 05:19:28 +0900

    Feat: Change text size function

--- uno/Text_Class.py
+++ uno/Text_Class.py
@@ -9,13 +9,17 @@ class Text:
         self.size = size
         self.color = color
         self.text = text
-        self.FONT = pg.font.SysFont(C.FONT, C.DEFAULT_SIZE)
+        self.FONT = pg.font.SysFont(C.FONT, self.size)
         self.ren = self.FONT.render(text, True, color)
         
     def draw(self, screen):
         screen.blit(self.ren, self.pos)
         
     def change_size(self, weight_idx):
-        self.FONT = pg.font.SysFont(C.FONT, int(C.DEFAULT_SIZE*C.WEIGHT[weight_idx]))
+        self.FONT = pg.font.SysFont(C.FONT, int(self.size*C.WEIGHT[weight_idx]))
         self.pos = tuple(int(item*C.WEIGHT[weight_idx]) for item in self.default_pos)
+        self.ren = self.FONT.render(self.text, True, self.color)
+
+    def change_text(self, text):
+        self.text = text
         self.ren = self.FONT.render(self.text, True, self.color)
