4877da3cf53683c4e4f36218197abd30b4469514
Han Jeong Hyeon <113430214+p1432pop@users.noreply.github.com>
2023-04-04 15:36:38 +0900

    Fix: Click Card

--- uno/CardButton_Class.py
+++ uno/CardButton_Class.py
@@ -2,7 +2,7 @@ import pygame as pg
 import uno.Constants as C
 
 class CardButton:
-    def __init__(self, card_name, card_color, card_type, function = None): # pos = 버튼 중앙 좌표, size = (가로, 세로), button_text = 내용
+    def __init__(self, card_name, img, function = None): # pos = 버튼 중앙 좌표, size = (가로, 세로), button_text = 내용
         #self.default_pos = pos
         #self.pos = pos
         #self.size = size
@@ -10,9 +10,8 @@ class CardButton:
         self.above = False
         self.on_key = False
         self.rect = pg.Rect(0, 0, 0, 0)
-        self.img = pg.transform.scale(pg.image.load("./assets/images/"+ card_name + ".png"), (45,90))
-        self.card_color = card_color
-        self.card_type = card_type
+        self.img = pg.transform.scale(img, (45,90))
+        self.card_name = card_name
 
     def update(self, mouse_pos):
         if self.rect.collidepoint(mouse_pos):
@@ -25,7 +24,7 @@ class CardButton:
         screen.blit(self.img, (x,y))
 
     def click(self, params = None):
-        print(self.card_color + self.card_type)
+        print(self.card_name)
         
         '''if self.function:
             if params:
@@ -40,9 +39,4 @@ class CardButton:
         self.text = self.FONT.render(self.button_text, True, self.text_color)
         self.text_rect = self.text.get_rect(center=self.pos)
         self.rect.center = self.pos
-        
-    def change_text(self, text):
-        self.button_text=text
-        self.text = self.FONT.render(text, True, C.BLACK)
-        self.text_rect = self.text.get_rect(center=self.pos)
         '''
