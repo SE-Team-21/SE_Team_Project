daedff1041623050897763b0a3b79eff2c8ff1e6
Han Jeong Hyeon <113430214+p1432pop@users.noreply.github.com>
2023-04-04 22:03:32 +0900

    Feat: Player Button Control

--- uno/Button_Class.py
+++ uno/Button_Class.py
@@ -2,7 +2,7 @@ import pygame as pg
 import uno.Constants as C
 
 class Button:
-    def __init__(self, pos, size, button_text, function = None, color = C.BLACK): # pos = 버튼 중앙 좌표, size = (가로, 세로), button_text = 내용
+    def __init__(self, pos, size, button_text, function = None, color = C.BLACK, locate = False): # pos = 버튼 중앙 좌표, size = (가로, 세로), button_text = 내용
         pg.font.init()
         self.default_pos = pos
         self.pos = pos
@@ -18,6 +18,9 @@ class Button:
         self.function = function
         self.above = False
         self.on_key = False
+        self.INACTIVE_COLOR = C.INACTIVE_COLOR
+        self.ABOVE_COLOR = C.ABOVE_COLOR
+        self.locate = locate
     
     def update(self, mouse_pos):
         if self.rect.collidepoint(mouse_pos):
@@ -27,9 +30,9 @@ class Button:
 
     def draw(self, screen):
         if self.above or self.on_key:
-            self.color = C.ABOVE_COLOR
+            self.color = self.ABOVE_COLOR
         else:
-            self.color = C.INACTIVE_COLOR
+            self.color = self.INACTIVE_COLOR
         pg.draw.rect(screen, self.color, self.rect)
         screen.blit(self.text, self.text_rect)
 
@@ -40,15 +43,21 @@ class Button:
             else:
                 self.function()
 
+    def location(self):
+        if self.locate:
+            self.text_rect = self.text.get_rect(topleft=(self.pos[0]-self.size[0]//2, self.pos[1]-self.size[1]//2))
+        else:
+             self.text_rect = self.text.get_rect(center=self.pos)
+
     def change_size(self, weight_idx):
         self.FONT = pg.font.SysFont(C.FONT, int(C.DEFAULT_SIZE*C.WEIGHT[weight_idx]))
         self.rect = pg.Rect(0, 0, *tuple(int(item*C.WEIGHT[weight_idx]) for item in self.size))
         self.pos = tuple(int(item*C.WEIGHT[weight_idx]) for item in self.default_pos)
         self.text = self.FONT.render(self.button_text, True, self.text_color)
-        self.text_rect = self.text.get_rect(center=self.pos)
+        self.location()
         self.rect.center = self.pos
         
     def change_text(self, text):
         self.button_text=text
         self.text = self.FONT.render(text, True, C.BLACK)
-        self.text_rect = self.text.get_rect(center=self.pos)
+        self.location()
