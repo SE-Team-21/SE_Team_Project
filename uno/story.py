f1fd7fef30099214f0b0c174375f7e1aaf2ee274
Han Jeong Hyeon <113430214+p1432pop@users.noreply.github.com>
2023-04-15 06:42:21 +0900

    Update story.py

--- uno/story.py
+++ uno/story.py
@@ -4,6 +4,7 @@ import pygame as pg
 from uno.Button_Class import Button
 from uno.play import Playing
 from uno.game.Game import UnoGame
+from uno.KeySettings import Data
 
 class Story(Display):
     def __init__(self):
@@ -16,7 +17,12 @@ class Story(Display):
         self.Button_list.append(Button((400, 500), (120, 60), 'Back', lambda x,y: self.next_screen(x,y)))
         self.backgroundimg = pg.transform.scale(pg.image.load("./assets/images/story_map.png"), C.DISPLAY_SIZE[Display.display_idx])
         self.cloudimg = pg.transform.scale(pg.image.load("./assets/images/cloud.png"), (int(444*C.WEIGHT[Display.display_idx]), int(300*C.WEIGHT[Display.display_idx])))
+        self.area1 = pg.transform.scale(pg.image.load("./assets/images/area1.png"), (int(180*C.WEIGHT[Display.display_idx]), int(341*C.WEIGHT[Display.display_idx])))
+        self.area2 = pg.transform.scale(pg.image.load("./assets/images/area2.png"), (int(180*C.WEIGHT[Display.display_idx]), int(351*C.WEIGHT[Display.display_idx])))
+        self.area3 = pg.transform.scale(pg.image.load("./assets/images/area3.png"), (int(180*C.WEIGHT[Display.display_idx]), int(330*C.WEIGHT[Display.display_idx])))
         self.active = [True, False, False, False]
+        for i in range(Data.data.Story):
+            self.active[i]=True
 
     def next_screen(self, idx, running):
         if idx==0:
@@ -25,6 +31,8 @@ class Story(Display):
     def next_screen_2(self, idx, running):
         if Playing.game == None:
                 Playing.game = UnoGame(5) # game inst
+        else:
+            Playing.reset()
         if idx==0:
             self.mode[C.NEXT_SCREEN] = C.PLAYING
         if idx==1:
@@ -45,9 +53,12 @@ class Story(Display):
         self.update_area(pg.mouse.get_pos())
         self.backgroundimg = pg.transform.scale(self.backgroundimg, C.DISPLAY_SIZE[Display.display_idx])
         self.screen.blit(self.backgroundimg, (0, 0))
-        self.screen.blit(self.cloudimg, (int(50*C.WEIGHT[Display.display_idx]), int(160*C.WEIGHT[Display.display_idx])))
-        self.screen.blit(self.cloudimg, (int(370*C.WEIGHT[Display.display_idx]), int(170*C.WEIGHT[Display.display_idx])))
-        self.screen.blit(self.cloudimg, (int(310*C.WEIGHT[Display.display_idx]), 0))
+        #self.screen.blit(self.cloudimg, (int(50*C.WEIGHT[Display.display_idx]), int(160*C.WEIGHT[Display.display_idx])))
+        #self.screen.blit(self.cloudimg, (int(370*C.WEIGHT[Display.display_idx]), int(170*C.WEIGHT[Display.display_idx])))
+        #self.screen.blit(self.cloudimg, (int(310*C.WEIGHT[Display.display_idx]), 0))
+        self.screen.blit(self.area1, (71, 56))
+        self.screen.blit(self.area2, (215, 260))
+        self.screen.blit(self.area3, (478, 56))
         self.update_screen(pg.mouse.get_pos())
         for event in pg.event.get():
             if event.type == pg.QUIT:
