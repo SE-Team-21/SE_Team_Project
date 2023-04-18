df3a3c5be97a25bf1bdd466c894e2b931fc07418
p1432pop <p1432pop@naver.com>
2023-04-18 14:33:22 +0900

    a

--- uno/game/Game.py
+++ uno/game/Game.py
@@ -167,6 +167,7 @@ class UnoGame:
         else:
             winner_name = self.players.index(self.winner)
         print("Player {} wins!".format(winner_name))
+        self.is_active = False
 
     def _pick_up(self, player, n):
         player.uno_state = False
