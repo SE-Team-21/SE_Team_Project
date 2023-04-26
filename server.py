acdffd77193f3d2f60ccdba1e6c0ea5b4a9572a6
p1432pop <p1432pop@naver.com>
2023-04-26 16:25:43 +0900

    1.2

--- server.py
+++ server.py
@@ -9,29 +9,42 @@ def Check(input, output): # 패스워드 확인
 
 serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(('', 10123))
-password = None
 serverSock.listen(3)
 players = []
+room = []
 num = 0
+
+class Room:
+    def __init__(self, pw, add, top):
+        self.password = pw
+        self.address = add
+        self.players = []
+        self.players.append(top)
+
+
 while True:
     readable, _, _ = select.select([serverSock], [], [], 0)
     for sock in readable:
         if sock is serverSock:
-            connection, address = serverSock.accept()
-            players.append(connection)
+            #connection, address = serverSock.accept()
+            players.append(serverSock.accept())
             num += 1
 
     for player in players:
-        readable, _, _ = select.select([player], [], [], 0)
+        readable, _, _ = select.select([player[0]], [], [], 0)
         try:
             for sock in readable:
                 data = E.Decrypt_A(sock.recv(1024))
-                if data["Type"] == "pw":
-                    if Check(data["Password"], password):
-                        player.send(str(datetime.now()).encode('utf-8'))
+                if data["Type"] == "mkr":
+                    room.append(Room(data["Password"], player[1][0], player))
+                    player[0].send(player[1][0].encode('utf-8'))
+                elif data["Type"] == "pw":
+                    if Check(data["Password"], room[0].password):
+                        player[0].send(str(datetime.now()).encode('utf-8'))
                     else:
-                        player.send('Password initialize or incorrect'.encode('utf-8'))
-                    if password == None:
-                        password = data["Password"]
+                        player[0].send('Password initialize or incorrect'.encode('utf-8'))
+                elif data["Type"] == "grs":
+                    player[0].send(player[1][0].encode('utf-8'))
+                    pass
         except:
             players.remove(player)
--- uno/multi.py
+++ uno/multi.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from uno.display import Display
 import uno.Constants as C
 import pygame as pg
+import time
 from uno.KeySettings import Data
 from uno.Button_Class import Button
 from uno.Text_Class import Text
@@ -15,6 +16,7 @@ class Multi(Display):
         self.Button_list.append(Button((300, 150), (120, 60), 'Connect Room', self.send))
         self.Button_list.append(Button((500, 150), (120, 60), '3', self.send))
         self.Text_list.append(Text((690, 160), 20, '', C.BLACK))
+        self.connection = False
         self.clientSock = None
         self.password = ''
         self.input_active = False
@@ -22,7 +24,8 @@ class Multi(Display):
 
     def connect(self):
         self.clientSock = socket(AF_INET, SOCK_STREAM)
-        self.clientSock.connect(('13.210.238.200', 10123))
+        #self.clientSock.connect(('13.210.238.200', 10123))
+        self.clientSock.connect(('127.0.0.1', 10123))
         self.clientSock.setblocking(False)
         print('연결 확인 됐습니다.')
     
@@ -30,8 +33,11 @@ class Multi(Display):
         self.clientSock.send(E.Encrypt_A({"Type": "exit"}))
         self.clientSock = None
 
-    def send(self, password): # 방 Password 전송
+    def send(self, password): # 방 Password 전송 / 입장용
         self.clientSock.send(E.Encrypt_A({"Type": "pw", "Password": E.Encrypt_S(password)}))
+
+    def make_room(self, password): # 방 생성
+        self.clientSock.send(E.Encrypt_A({"Type": "mkr", "Password": E.Encrypt_S(password)}))
     
     def get_room_state(self): # 대기실 상태 요청
         self.clientSock.send(E.Encrypt_A({"Type": "grs"}))
@@ -39,48 +45,73 @@ class Multi(Display):
     def get_game_state(self): # 게임 진행 상태 요청
         self.clientSock.send(E.Encrypt_A({"Type": "ggs"}))
 
-
+    def draw_connect_motion(self):
+        self.screen.fill((255, 255, 255))
+        text = Text((400, 300), 40, 'Connecting .', C.BLACK)
+        text.draw(self.screen)
+        pg.display.update()
+        time.sleep(1)
+        text.change_text('Connecting ..')
+        text.draw(self.screen)
+        pg.display.update()
+        time.sleep(1)
+        text.change_text('Connecting ...')
+        text.draw(self.screen)
+        pg.display.update()
+        time.sleep(1)
+        text.change_text('Connecting .')
+        text.draw(self.screen)
+        pg.display.update()
+        time.sleep(1)
 
     def next_screen(self, idx, running):
         pass
 
     def main_loop(self, running):
-        self.screen.fill((255, 255, 255))
-        self.pw_input_box.update(pg.mouse.get_pos())
-        self.pw_input_box.draw(self.screen)
-        self.update_screen(pg.mouse.get_pos())
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running[0] = False
-                return
-            elif event.type == pg.MOUSEBUTTONDOWN:
-                for idx, item in enumerate(self.Button_list):
-                    if item.above:
-                        item.click()
-                if self.pw_input_box.above:
-                            self.input_active = True
-            elif event.type == pg.KEYUP:
-                    if self.input_active:
-                        if event.key == pg.K_BACKSPACE:
-                            self.password = self.password[:-1]
-                            temp = ''
-                            for i in self.password:
-                                temp+='*'
-                            self.pw_input_box.change_text(temp)
-                        elif event.key == pg.K_RETURN and len(self.password)>=6:
-                            self.input_active = False
-                            self.send(self.password)
-                            print(self.password)
-                            self.password = ''
-                        elif (event.unicode.isalpha() or event.unicode.isdigit()) and len(self.password)<=12:
-                            self.password += event.unicode
-                            temp = ''
-                            for i in self.password:
-                                temp+='*'
-                            self.pw_input_box.change_text(temp)
-        if self.clientSock is not None:
+        if self.connection is False: # 서버 연결
+            self.draw_connect_motion()
             try:
-                data = self.clientSock.recv(1024).decode('utf-8')
-                print('받은 데이터 : ', data)
+                self.connect()
+                self.connection = True
             except:
                 pass
+        else:
+            self.pw_input_box.update(pg.mouse.get_pos())
+            self.screen.fill((255, 255, 255))
+            self.pw_input_box.draw(self.screen)
+            self.update_screen(pg.mouse.get_pos())
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running[0] = False
+                    return
+                elif event.type == pg.MOUSEBUTTONDOWN:
+                    for idx, item in enumerate(self.Button_list):
+                        if item.above:
+                            item.click()
+                    if self.pw_input_box.above:
+                                self.input_active = True
+                elif event.type == pg.KEYUP:
+                        if self.input_active:
+                            if event.key == pg.K_BACKSPACE:
+                                self.password = self.password[:-1]
+                                temp = ''
+                                for i in self.password:
+                                    temp+='*'
+                                self.pw_input_box.change_text(temp)
+                            elif event.key == pg.K_RETURN and len(self.password)>=6:
+                                self.input_active = False
+                                self.make_room(self.password)
+                                print(self.password)
+                                self.password = ''
+                            elif (event.unicode.isalpha() or event.unicode.isdigit()) and len(self.password)<=12:
+                                self.password += event.unicode
+                                temp = ''
+                                for i in self.password:
+                                    temp+='*'
+                                self.pw_input_box.change_text(temp)
+            if self.clientSock is not None:
+                try:
+                    data = self.clientSock.recv(1024).decode('utf-8')
+                    print('받은 데이터 : ', data)
+                except:
+                    pass
